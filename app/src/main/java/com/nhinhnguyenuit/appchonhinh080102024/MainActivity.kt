8a6969ca4e563cfb2848444449a6e1622cc1f34a
StevenNguyenUIT <143907704+StevenNguyenUIT@users.noreply.github.com>
2024-08-09 07:07:39 -0500

    update arrays String

--- app/src/main/java/com/nhinhnguyenuit/appchonhinh080102024/MainActivity.kt
+++ app/src/main/java/com/nhinhnguyenuit/appchonhinh080102024/MainActivity.kt
@@ -1,52 +1,40 @@
 package com.nhinhnguyenuit.appchonhinh080102024
 
-import android.annotation.SuppressLint
-import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import java.util.Random
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var imvRandom: ImageView
-    private var listImage: MutableList<Int> = mutableListOf(
-        R.drawable.bo,
-        R.drawable.bocanhcung,
-        R.drawable.bongua,
-        R.drawable.cachep,
-        R.drawable.cadia,
-        R.drawable.chimcanhcut,
-        R.drawable.cho,
-        R.drawable.chodom,
-        R.drawable.chuonchuon,
-        R.drawable.ech,
-        R.drawable.heo,
-        R.drawable.khi,
-        R.drawable.meoden,
-        R.drawable.meolucky,
-        R.drawable.meotrang,
-        R.drawable.rua,
-        R.drawable.soi,
-        R.drawable.voi
-    )
+    private var imgRandom: ImageView? = null
+    private var imgUserSelect: ImageView? = null
+    private val arrNameAnimals by lazy {
+        resources.getStringArray(R.array.array_animal)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
 
-        imvRandom = findViewById(R.id.image_view_random)
-        imvRandom.setOnClickListener {
-            val randomIndex = kotlin.random.Random.nextInt(listImage.size)
-            imvRandom.setImageResource(listImage[randomIndex])
-        }
+        imgRandom = findViewById(R.id.image_view_random)
+        imgUserSelect = findViewById(R.id.image_view_user_select)
+        setImageRandom(imgRandom)
+    }
+
+    private fun setImageRandom(imageView: ImageView?) {
+        imageView ?: return
+        val indexRandom = Random.nextInt(arrNameAnimals.size)
+        val resourceRandom =
+            resources.getIdentifier(arrNameAnimals[indexRandom], "drawable", packageName)
+        imgRandom?.setImageResource(resourceRandom)
     }
 }
